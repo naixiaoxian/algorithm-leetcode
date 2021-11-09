a19ed30a1522281cdfd14b1985a77cb22e0e1248
凯多 <421980072@qq.com>
2021-11-09 09:09:54 +0800

    no message

---
+++ go-version/47/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+fmt.Println(permuteUnique([]int{1,2,3,4,5}))
+}
+
+func permuteUnique(nums []int) (ans [][]int) {
+	sort.Ints(nums)
+	n := len(nums)
+	var perm []int
+	vis := make([]bool, n)
+	var backtrack func(int)
+	backtrack = func(idx int) {
+		if idx == n {
+			ans = append(ans, append([]int(nil), perm...))
+			return
+		}
+		for i, v := range nums {
+			if vis[i] || i > 0 && !vis[i-1] && v == nums[i-1] {
+				continue
+			}
+			perm=append(perm,v)
+			vis[i]=true
+			backtrack(idx+1)
+			vis[i] = false
+			perm=perm[:len(perm)-1]
+		}
+	}
+	backtrack(0)
+	return
+}
